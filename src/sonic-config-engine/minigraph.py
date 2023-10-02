b98b8d5a90e69ca6563641a01ddb3caa504a7e20
Rida Hanif <61490193+ridahanif96@users.noreply.github.com>
2023-10-02 21:10:50 +0000

    fix

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -1837,11 +1837,11 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
                 # for the ports w/o neighbor info, set it to port alias
                 port['description'] = port.get('alias', port_name)
 
-    # set default port MTU as 9100 and default TPID 0x8100 and default mode trunk
+    # set default port MTU as 9100 and default TPID 0x8100 and default mode routed
     for port in ports.values():
         port['mtu'] = '9100'
         port['tpid'] = '0x8100'
-        port['mode'] = 'trunk'
+        port['mode'] = 'routed'
 
     # asymmetric PFC is disabled by default
     for port in ports.values():
@@ -1908,11 +1908,11 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
                 for pc_mbr_del_key in pc_mbr_del_keys:
                     del pc_members[pc_mbr_del_key]
 
-    # set default port channel MTU as 9100 and admin status up and default TPID 0x8100 and mode default trunk
+    # set default port channel MTU as 9100 and admin status up and default TPID 0x8100 and mode default routed
     for pc in pcs.values():
         pc['mtu'] = '9100'
         pc['tpid'] = '0x8100'
-        pc['mode'] = 'trunk'
+        pc['mode'] = 'routed'
         pc['admin_status'] = 'up'
 
     results['PORTCHANNEL'] = pcs
