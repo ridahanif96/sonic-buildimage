e31f1b9a6373506c0021b8f2fa296696f13daa93
Rida Hanif <61490193+ridahanif96@users.noreply.github.com>
2023-10-27 09:27:38 +0000

    Fixx

--- src/sonic-config-engine/tests/test_minigraph_case.py
+++ src/sonic-config-engine/tests/test_minigraph_case.py
@@ -158,7 +158,7 @@ class TestCfgGenCaseInsensitive(TestCase):
         output = self.run_script(argument)
         self.assertEqual(
             utils.to_dict(output.strip()),
-            utils.to_dict("{'PortChannel01': {'admin_status': 'up', 'min_links': '1', 'mtu': '9100', 'tpid': '0x8100', 'lacp_key': 'auto', 'mode': 'routed'}}")
+            utils.to_dict("{'PortChannel01': {'admin_status': 'up', 'min_links': '1', 'mtu': '9100', 'tpid': '0x8100', 'lacp_key': 'auto'}}")
         )
 
     def test_minigraph_console_mgmt_feature(self):
@@ -547,7 +547,6 @@ class TestCfgGenCaseInsensitive(TestCase):
         mgmt_intf = result['MGMT_INTERFACE']
         self.assertEqual(len(mgmt_intf.keys()), 1)
         self.assertTrue(('eth0', 'FC00:1::32/64') in mgmt_intf.keys())
-
         self.assertTrue(ipaddress.ip_address(u'fc00:1::1') == mgmt_intf[('eth0', 'FC00:1::32/64')]['gwaddr'])
 
     def test_mgmt_device_disable_counters(self):
